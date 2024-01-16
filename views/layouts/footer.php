3a6a2d1bb69295793bf6431965ccbd97d4419375
mtoolianz <mtoolianz@gmail.com>
2024-01-16 09:12:31 +0530

    auth file added

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -6,6 +6,19 @@
                 </div>
             </footer>
             <!-- / Footer -->
+
+            <div class="content-backdrop fade"></div>
+            </div>
+            <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+            </div>
+
+            <!-- Overlay -->
+            <div class="layout-overlay layout-menu-toggle"></div>
+            </div>
+            <!-- / Layout wrapper -->
+
             <!-- Core JS -->
             <!-- build:js assets/vendor/js/core.js -->
             <script src="<?= asset('assets/vendor/libs/jquery/jquery.js') ?>"></script>
